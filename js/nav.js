f520f87a0d247f890ea6df061d9968ba5f24570c
kunk80110 <kunk80110@gmail.com>
2022-10-22 14:58:55 +0200

    fixed behavior of nav-PC

--- js/nav.js
+++ js/nav.js
@@ -1,44 +1,51 @@
+const btnsAmbully = [
+  document.querySelector('.ambully1'),
+  document.querySelector('.ambully2'),
+  document.querySelector('.ambully3'),
+];
+const btnsAmstaff = [
+  document.querySelector('.amstaff1'),
+  document.querySelector('.amstaff2'),
+  document.querySelector('.amstaff3'),
+];
+const btnsFrench = [
+  document.querySelector('.french1'),
+  document.querySelector('.french2'),
+  document.querySelector('.french3'),
+];
+
 function showAmstf() {
-  let btns = [
-    document.querySelector('.amstaff1'),
-    document.querySelector('.amstaff2'),
-    document.querySelector('.amstaff3'),
-  ];
-  for (let i = 0; i < btns.length; i++) {
-    if (btns[i].style.display == 'none') {
-      btns[i].style.display = 'block';
+  for (let i = 0; i < btnsAmstaff.length; i++) {
+    if (
+      !btnsAmstaff[i].style.display ||
+      btnsAmstaff[i].style.display == 'none'
+    ) {
+      btnsAmstaff[i].style.display = 'block';
     } else {
-      btns[i].style.display = 'none';
+      btnsAmstaff[i].style.display = 'none';
     }
   }
 }
 
 function showAmBully() {
-  let btns = [
-    document.querySelector('.ambully1'),
-    document.querySelector('.ambully2'),
-    document.querySelector('.ambully3'),
-  ];
-  for (let i = 0; i < btns.length; i++) {
-    if (btns[i].style.display == 'none') {
-      btns[i].style.display = 'block';
+  for (let i = 0; i < btnsAmbully.length; i++) {
+    if (
+      !btnsAmbully[i].style.display ||
+      btnsAmbully[i].style.display == 'none'
+    ) {
+      btnsAmbully[i].style.display = 'block';
     } else {
-      btns[i].style.display = 'none';
+      btnsAmbully[i].style.display = 'none';
     }
   }
 }
 
 function showFrench() {
-  let btns = [
-    document.querySelector('.french1'),
-    document.querySelector('.french2'),
-    document.querySelector('.french3'),
-  ];
-  for (let i = 0; i < btns.length; i++) {
-    if (btns[i].style.display == 'none') {
-      btns[i].style.display = 'block';
+  for (let i = 0; i < btnsFrench.length; i++) {
+    if (!btnsFrench[i].style.display || btnsFrench[i].style.display == 'none') {
+      btnsFrench[i].style.display = 'block';
     } else {
-      btns[i].style.display = 'none';
+      btnsFrench[i].style.display = 'none';
     }
   }
 }
